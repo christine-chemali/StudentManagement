f4e4d67cb0183dfe7ae5f1371aba044b83cbe5fa
christine chemali <christine.chemali@laplateforme.io>
2025-07-17 21:19:27 +0200

    :memo: docs: Add Javadoc comments to AlertUtils class for improved documentation
    - Enhanced the documentation of the AlertUtils class by adding Javadoc comments to methods.
    - Each method now includes a description of its purpose, parameters, and behavior.
    - Improved clarity and maintainability of the code, making it easier for developers to understand the functionality. :alien:

--- src/main/java/com/studentmanagement/utils/AlertUtils.java
+++ src/main/java/com/studentmanagement/utils/AlertUtils.java
@@ -9,6 +9,10 @@ public class AlertUtils {
     //Path to Css file
     private static final String CSS_FILE = "/css/style.css";
 
+    /**
+     * Applies custim styling to the specified alert dialog
+     * @param alert the alert to which the custom style will be applied
+     */
     private static void applyCustomStyle(Alert alert){
         try {
             DialogPane dialogPane = alert.getDialogPane();
@@ -27,6 +31,11 @@ public class AlertUtils {
         }
     }
 
+    /**
+     * Displays an alert dialog with the specified title and message
+     * @param title the title of the alert dialog
+     * @param message the message to be displayed in the alert dialog
+     */
     public static void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -36,34 +45,46 @@ public class AlertUtils {
         alert.showAndWait();
     }
 
-//Error alert message   
-public static void showError(String  title, String message) {
-    Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setTitle(title);
-    alert.setHeaderText(null);
-    alert.setContentText(message);
-    applyCustomStyle(alert);
-    alert.showAndWait();
-}
+    /**
+     * Displays an error alert dialog with the specified title and message
+     * @param title the title of the error alert dialog
+     * @param message the message to be displayed in the error alert dialog
+     */   
+    public static void showError(String  title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        applyCustomStyle(alert);
+        alert.showAndWait();
+    }
 
-//Warning alert message
-public static void showWarning(String title, String message){
-    Alert alert = new Alert(Alert.AlertType.WARNING);
-    alert.setTitle(title);
-    alert.setHeaderText(null);
-    alert.setContentText(message);
-    applyCustomStyle(alert);
-    alert.showAndWait();
-}
+    /**
+     * Displays a warning alert dialog with the specified title and message
+     * @param title the title of the warning alert dialog
+     * @param message the message to be displayed in the warning alert dialog
+     */
+    public static void showWarning(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        applyCustomStyle(alert);
+        alert.showAndWait();
+    }
 
-//Information alert message
-public static void showInformation(String title, String message){
-    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle(title);
-    alert.setHeaderText(null);
-    alert.setContentText(message);
-    applyCustomStyle(alert);
-    alert.showAndWait();
-}
+    /**
+     * Displays an information alert dialog with the specified title and message
+     * @param title the title of the information alert dialog
+     * @param message the message to be displayed in the information alert dialog
+     */
+    public static void showInformation(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        applyCustomStyle(alert);
+        alert.showAndWait();
+    }
 
 }
